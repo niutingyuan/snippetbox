4f8e246dc9e833dac62cb6b959930f103cbe3d6a
niutingyuan <thomasniu@gmail.com>
2024-06-14 02:16:49 +0800

    Add form validation for creating snippets
    The code has been updated to include form validation checks when creating new snippets. A new struct, snippetCreateForm, was added to represent the form data and validation errors. Field values are now checked for correctness and feedback is provided through the display of validation errors.

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -52,6 +52,14 @@ func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
 	app.render(w, r, http.StatusOK, "create.tmpl", data)
 }
 
+// Define a snippetCreateForm struct to represent the form data and validation errors for the form fields. Note that all the struct fields are deliberately exported (i.e. start with a capital letter). This is because struct fields must be exported in order to be read by the html/template package when rendering the template.
+type snippetCreateForm struct {
+	Title       string
+	Content     string
+	Expires     int
+	FieldErrors map[string]string
+}
+
 func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request) {
 	err := r.ParseForm()
 	if err != nil {
@@ -59,42 +67,45 @@ func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request
 		return
 	}
 
-	title := r.PostForm.Get("title")
-	content := r.PostForm.Get("content")
-
+	// Get the expires value from the form as normal.
 	expires, err := strconv.Atoi(r.PostForm.Get("expires"))
 	if err != nil {
 		app.clientError(w, http.StatusBadRequest)
 		return
 	}
 
-	// Initialize a map to hold any validation errors for the form fields.
-	fieldErrors := make(map[string]string)
-
-	// Check that the title value is not blank and is not more than 100 characters long. If it fails either of those checks, add a message to the errors map using the field name as th key.
-	if strings.TrimSpace(title) == "" {
-		fieldErrors["title"] = "This field cannot be bland"
-	} else if utf8.RuneCountInString(title) > 100 {
-		fieldErrors["title"] = "This field cannot be more than 100 characters long"
+	// Create an instance of the snippetCreateForm struct containing the values from the form and an empty map for any validation errors.
+	form := snippetCreateForm{
+		Title:       r.PostForm.Get("Title"),
+		Content:     r.PostForm.Get("content"),
+		Expires:     expires,
+		FieldErrors: map[string]string{},
 	}
 
-	// Check that the Content value isn't blank.
-	if strings.TrimSpace(content) == "" {
-		fieldErrors["content"] = "This field cannot be blank"
+	// Update the validation checks so that they operate on the snippetCreateForm instance.
+	if strings.TrimSpace(form.Title) == "" {
+		form.FieldErrors["Title"] = "This field cannot be bland"
+	} else if utf8.RuneCountInString(form.Title) > 100 {
+		form.FieldErrors["Title"] = "This field cannot be more than 100 characters long"
+	}
+
+	if strings.TrimSpace(form.Content) == "" {
+		form.FieldErrors["content"] = "This field cannot be blank"
 	}
 
 	// Check the expires value matches one of the permitted values (1, 7, or 365).
 	if expires != 1 && expires != 7 && expires != 365 {
-		fieldErrors["expires"] = "This field must be equal 1, 7, or 365"
+		form.FieldErrors["expires"] = "This field must be equal 1, 7, or 365"
 	}
 
-	// If there are any errors, dump them in a plain text HTTP response and return from the handler.
-	if len(fieldErrors) > 0 {
-		fmt.Fprint(w, fieldErrors)
-		return
+	// If there are any validation errors, then re-display the create.tmpl template, passing in the snippetCeateForm instance as dynamic data in the Form field. Note that we use the HTTP status code 422 Unprocessable Entity when sending the response to indicate that there was a validation error.
+	if len(form.FieldErrors) > 0 {
+		data := app.newTemplateData(r)
+		data.Form = form
+		app.render(w, r, http.StatusUnprocessableEntity, "create.tmpl", data)
 	}
 
-	id, err := app.snippets.Insert(title, content, expires)
+	id, err := app.snippets.Insert(form.Title, form.Content, form.Expires)
 	if err != nil {
 		app.serverError(w, r, err)
 		return
