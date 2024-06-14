41e7b84acdb9f409256503cbf6374e4cf5651518
Thomas Niu <thomas@gentrice.net>
2024-06-14 15:01:05 +0800

    Refactor form handling in web handlers
    This commit moves the snippetCreateForm struct up in the file and initializes it with default values in the snippetCreate function, to improve code organization. It also modifies field naming in form validation code and templates, switching from "Title" to "title", to improve consistency. The create template was updated too, to show form field errors and repopulate user input.

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -5,18 +5,9 @@ import (
 	"fmt"
 	"net/http"
 	"snippetbox.thomas.net/internal/models"
+	"snippetbox.thomas.net/internal/validator"
 	"strconv"
-	"strings"
-	"unicode/utf8"
 )
-
-// Define a snippetCreateForm struct to represent the form data and validation errors for the form fields. Note that all the struct fields are deliberately exported (i.e. start with a capital letter). This is because struct fields must be exported in order to be read by the html/template package when rendering the template.
-type snippetCreateForm struct {
-	Title       string
-	Content     string
-	Expires     int
-	FieldErrors map[string]string
-}
 
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	snippets, err := app.snippets.Latest()
@@ -65,6 +56,13 @@ func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
 	app.render(w, r, http.StatusOK, "create.tmpl", data)
 }
 
+type snippetCreateForm struct {
+	Title   string
+	Content string
+	Expires int
+	validator.Validator
+}
+
 func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request) {
 	err := r.ParseForm()
 	if err != nil {
@@ -81,33 +79,21 @@ func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request
 
 	// Create an instance of the snippetCreateForm struct containing the values from the form and an empty map for any validation errors.
 	form := snippetCreateForm{
-		Title:       r.PostForm.Get("title"),
-		Content:     r.PostForm.Get("content"),
-		Expires:     expires,
-		FieldErrors: map[string]string{},
+		Title:   r.PostForm.Get("title"),
+		Content: r.PostForm.Get("content"),
+		Expires: expires,
 	}
 
-	// Update the validation checks so that they operate on the snippetCreateForm instance.
-	if strings.TrimSpace(form.Title) == "" {
-		form.FieldErrors["title"] = "This field cannot be bland"
-	} else if utf8.RuneCountInString(form.Title) > 100 {
-		form.FieldErrors["title"] = "This field cannot be more than 100 characters long"
-	}
+	form.CheckField(validator.NotBlank(form.Title), "title", "This field cannot be blank")
+	form.CheckField(validator.MaxChars(form.Title, 100), "title", "This field cannot be more than 100 characters long")
+	form.CheckField(validator.NotBlank(form.Content), "content", "This field cannot be blank")
+	form.CheckField(validator.PermittedValue(form.Expires, 1, 7, 365), "expires", "This field must equal 1, 7 or 365")
 
-	if strings.TrimSpace(form.Content) == "" {
-		form.FieldErrors["content"] = "This field cannot be blank"
-	}
-
-	// Check the expires value matches one of the permitted values (1, 7, or 365).
-	if expires != 1 && expires != 7 && expires != 365 {
-		form.FieldErrors["expires"] = "This field must be equal 1, 7, or 365"
-	}
-
-	// If there are any validation errors, then re-display the create.tmpl template, passing in the snippetCeateForm instance as dynamic data in the Form field. Note that we use the HTTP status code 422 Unprocessable Entity when sending the response to indicate that there was a validation error.
-	if len(form.FieldErrors) > 0 {
+	if !form.Valid() {
 		data := app.newTemplateData(r)
 		data.Form = form
 		app.render(w, r, http.StatusUnprocessableEntity, "create.tmpl", data)
+		return
 	}
 
 	id, err := app.snippets.Insert(form.Title, form.Content, form.Expires)
